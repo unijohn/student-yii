7f9cdb4d2db94445576d7756fe4468cc4f34785d
J Matthew Hall <jmhall@memphis.edu>
2020-07-14 14:47:45 -0500

    Moved the listing/view logic into the index page

--- views/users/index.php
+++ views/users/index.php
@@ -7,53 +7,17 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Framework | Users | Placeholder';
 
 $auth = Yii::$app->authManager;
 ?>
    <div class="site-about">
       <h1><?= Html::encode($this->title) ?></h1>
       <p>
-         This is the Framework | Users page. You may modify the following file to customize its content:
+         This is the <?php print( $this->title ); ?> page. You may modify the following file to customize its content:
       </p>
 
-      <div id=users-search-form'>      
-<?= $this->render('_users-search', ['model' => $model]); ?>      
-      </div>
-    
-      <div class="body-content">
-         <div class="row">
-         
-<?= 
-GridView::widget([
-    'dataProvider'   => $dataProvider,
-    'columns' => [
-//      [ 'class' => 'yii\grid\CheckboxColumn' ],
-      [
-         'attribute' => 'id',
-         'label'     => 'ID',
-      ],
-      [
-         'attribute' => 'uuid',
-         'label' => 'UUID',
-         'format' => 'raw',
-         'value' => function( $data ){
-            return HTML::a( $data['uuid'], Url::toRoute( ['users/view', 'uuid' => $data['uuid'] ], true) );
-         },
-      ],
-      'name',
-      
-      'created_at:datetime',
-      'updated_at:datetime',
-    
-    ],
-//    'showFooter' => false,
-//    'placeFooterAfterBody' => false,
-]); 
-?>  
-         </div>       
-
-         <code><?= __FILE__ ?></code>
+      <code><?= __FILE__ ?></code>
          
       </div>
    </div>
