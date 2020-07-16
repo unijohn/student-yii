dd1dc11ce2486a54118451bd719986b0bb187273
J Matthew Hall <jmhall@memphis.edu>
2020-07-16 12:44:16 -0500

    Initial creation of model files for the built-in authorization system

---
+++ models/AuthAssignment.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\model;
+use yii\rbac\DbManager;
+
+
+use app\models\User;
+use app\models\AuthItem;
+
+
+class AuthAssignment extends \yii\db\ActiveRecord
+{
+   public $item_name;
+   public $user_id;
+
+
+   public function init()
+   {
+      parent::init();
+   }   
+
+
+   public static function tableName()
+   {
+      return '{{auth_assignment}}';
+   }  
+    
+   
+   /**
+   * @inheritdoc
+   */
+   public function attributeLabels()
+   {
+   
+      return [
+//         'id' => Yii::t('app', 'ID'),
+//         'uuid'         => Yii::t('app', 'UUID'),
+//         '$is_active'   => Yii::t('app', 'Account Status'),
+   
+//         'auth_key' => Yii::t('app', 'Published'),
+//         'access_token' => Yii::t('app', 'Title'),
+//         'Description' => Yii::t('app', 'Description'),
+//         'Content' => Yii::t('app', 'Content'),
+//         'Format' => Yii::t('app', 'Format'),
+      ];
+   
+   }   
+
+   // explicitly list every field, best used when you want to make sure the changes
+   // in your DB table or model attributes do not cause your field changes (to keep API backward compatibility).
+   public function fields()
+   {
+
+/**
+      return [
+         // field name is the same as the attribute name
+         'id'     => 'id',
+      
+         // field name is "email", the corresponding attribute name is "email_address"
+         'uuid'   => 'uuid',
+   
+      ];
+ **/
+   }
+
+
+   /**
+   * @inheritdoc
+   */
+   public function behaviors()
+   {
+      return [     
+      ];
+   }
+
+
+   public function rules()
+   {
+      return [
+//         [['uuid', 'name', 'is_active', 'auth_key', 'access_token', 'created_at'], 'required' ],
+      ];
+   }
+   
+   
+   
+/**
+ *
+ *    Relationships & Model section
+ *
+ **/   
+   public function getUsers()
+   {
+      return $this->hasMany(User::className(), [ 'id' => 'user_id' ] );
+   } 
+
+
+   public function getRoles()
+   {
+      return $this->hasMany(AuthItem::className(), [ 'name' => 'item_name' ] );
+   }       
+}
---
+++ models/AuthItem.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace app\models;
+
+
+use Yii;
+use yii\base\model;
+use yii\rbac\DbManager;
+
+
+use app\models\AuthAssignment;
+
+
+class AuthItem extends \yii\db\ActiveRecord
+{
+   const TYPE_ROLE         = 1;
+   const TYPE_PERMISSION   = 2;
+
+
+   public $name;
+   public $type;
+   public $description;
+   public $created_at;
+   public $updated_at;
+
+
+   public function init()
+   {
+      parent::init();
+   }   
+
+
+   public static function tableName()
+   {
+      return '{{auth_item}}';
+   }  
+    
+   
+   /**
+   * @inheritdoc
+   */
+   public function attributeLabels()
+   {
+   
+      return [
+//         'id' => Yii::t('app', 'ID'),
+//         'uuid'         => Yii::t('app', 'UUID'),
+//         '$is_active'   => Yii::t('app', 'Account Status'),
+   
+//         'auth_key' => Yii::t('app', 'Published'),
+//         'access_token' => Yii::t('app', 'Title'),
+//         'Description' => Yii::t('app', 'Description'),
+//         'Content' => Yii::t('app', 'Content'),
+//         'Format' => Yii::t('app', 'Format'),
+      ];
+   
+   }   
+
+   // explicitly list every field, best used when you want to make sure the changes
+   // in your DB table or model attributes do not cause your field changes (to keep API backward compatibility).
+   public function fields()
+   {
+
+/**
+      return [
+         // field name is the same as the attribute name
+         'id'     => 'id',
+      
+         // field name is "email", the corresponding attribute name is "email_address"
+         'uuid'   => 'uuid',
+   
+      ];
+ **/
+   }
+
+
+   /**
+   * @inheritdoc
+   */
+   public function behaviors()
+   {
+      return [     
+      ];
+   }
+
+
+   /**
+   * @inheritdoc
+   */
+   public function rules()
+   {
+      return [
+         [['name', 'type', ], 'required' ],
+      ];
+   }
+
+   
+   public static function findbyRoles()
+   {
+      return AuthItem::find()
+         ->where(['type' => AuthItem::TYPE_ROLE ])
+         ->all();
+   }
+
+   public static function findbyUnassignedRoles( $assignedRoles = '' )
+   {
+      return AuthItem::find()
+         ->where([ 'type' => AuthItem::TYPE_ROLE ]) 
+         ->andWhere([ 'not in' , 'name', $assignedRoles ])
+         ->all();         
+   }   
+
+
+   public static function findbyPermissions()
+   {
+      return AuthItem::find()
+         ->where(['type' => AuthItem::TYPE_PERMISSION ])
+         ->all();
+   }
+   
+   
+/**
+ *
+ *    Relationships & Model section
+ *
+ **/   
+
+   public function getAssignments()
+   {
+      return $this->hasMany(AuthAssignment::className(), [ 'item_name' => 'name' ] );
+   }
+}
