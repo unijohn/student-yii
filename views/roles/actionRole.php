da26738d1aee943507c5bc93894d3c3a26dc56cb
J Matthew Hall <jmhall@memphis.edu>
2020-07-22 15:58:00 -0500

    Moved alert content into temporary view; may be removed now that that functionality is in the site/index view

--- views/roles/actionRole.php
+++ views/roles/actionRole.php
@@ -11,25 +11,60 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the <? print( $this->title ); ?> page. You may modify the following file to customize its content:
+        This is the <?php print( $this->title ); ?> page. You may modify the following file to customize its content:
     </p>
     
 <?php
    if( $data )
    {
+      if( isset( $data['errors'] ) && !empty( $data['errors'] ) )
+      {
+         print( "<div class='alert alert-danger' role='alert'> " . PHP_EOL );
+             
+         foreach( $data['errors'] as $key => $value )
+         {
+            $htmlTagOpen   = "";
+            $htmlTagClose  = "<br />";
+         
+            $errorName = $key;
+   
+            if( isset( $data['errors'][$key]['htmlTag'] ) && !empty( $data['errors'][$key]['htmlTag'] ) )
+            {
+               $htmlTagOpen   = "<"    . $data['errors'][$key]['htmlTag'] . ">";
+               $htmlTagClose  = "</"   . $data['errors'][$key]['htmlTag'] . ">";             
+            }
+    
+            print( $htmlTagOpen . "<strong>" . $errorName . " </strong> " . $data['errors'][$key]['value'] . $htmlTagClose . PHP_EOL);
+         }
+         
+         print( "</div> " . PHP_EOL );      
+      }
+      
+      if( isset( $data['success'] ) && !empty( $data['success'] ) )
+      {
+         print( "<div class='alert alert-success' role='alert'> " . PHP_EOL );
+             
+         foreach( $data['success'] as $key => $value )
+         {
+            $htmlTagOpen   = "";
+            $htmlTagClose  = "<br />";
+         
+            $errorName = $key;
+   
+            if( isset( $data['success'][$key]['htmlTag'] ) && !empty( $data['success'][$key]['htmlTag'] ) )
+            {
+               $htmlTagOpen   = "<"    . $data['success'][$key]['htmlTag'] . ">";
+               $htmlTagClose  = "</"   . $data['success'][$key]['htmlTag'] . ">";             
+            }
+            
+            print( $htmlTagOpen . "<strong>" . $errorName . " </strong> " . $data['success'][$key]['value'] . $htmlTagClose . PHP_EOL);
+         }
+         
+         print( "</div> " . PHP_EOL );      
+      }   
+
       echo ( "<Pre>" );
       print_r( $data );
-      echo ( "</Pre>" );
-
-      echo ( "<Pre>" );      
-      foreach( $data as $gRKey => $getRole )
-      {
-         foreach( $getRole as $rKey => $role )
-         {
-            print( $rKey . "] " . $role['type'] . " :: " . $role['description'] );
-//            print_r( $role );
-         }
-      }      
       echo ( "</Pre>" );
    }
  ?>
