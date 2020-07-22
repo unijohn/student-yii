96493db2f6ad4f799228d2339f949489d3ea1431
J Matthew Hall <jmhall@memphis.edu>
2020-07-22 16:27:21 -0500

    Removed lengthy alert div from view and moved into layout

--- views/site/index.php
+++ views/site/index.php
@@ -27,52 +27,7 @@ $auth = Yii::$app->authManager;
 
 <?php
    if( isset( $data ) && !empty( $data ) )
-   {
-      if( isset( $data['errors'] ) && !empty( $data['errors'] ) )
-      {
-         print( "<div class='alert alert-danger' role='alert'> " . PHP_EOL );
-             
-         foreach( $data['errors'] as $key => $value )
-         {
-            $htmlTagOpen   = "";
-            $htmlTagClose  = "<br />";
-         
-            $errorName = $key;
-   
-            if( isset( $data['errors'][$key]['htmlTag'] ) && !empty( $data['errors'][$key]['htmlTag'] ) )
-            {
-               $htmlTagOpen   = "<"    . $data['errors'][$key]['htmlTag'] . ">";
-               $htmlTagClose  = "</"   . $data['errors'][$key]['htmlTag'] . ">";             
-            }
-    
-            print( $htmlTagOpen . "<strong>" . $errorName . " </strong> " . $data['errors'][$key]['value'] . $htmlTagClose . PHP_EOL);
-         }
-         
-         print( "</div> " . PHP_EOL );      
-      }
-      
-      if( isset( $data['success'] ) && !empty( $data['success'] ) )
-      {
-         print( "<div class='alert alert-success' role='alert'> " . PHP_EOL );
-             
-         foreach( $data['success'] as $key => $value )
-         {
-            $htmlTagOpen   = "";
-            $htmlTagClose  = "<br />";
-         
-            $errorName = $key;
-   
-            if( isset( $data['success'][$key]['htmlTag'] ) && !empty( $data['success'][$key]['htmlTag'] ) )
-            {
-               $htmlTagOpen   = "<"    . $data['success'][$key]['htmlTag'] . ">";
-               $htmlTagClose  = "</"   . $data['success'][$key]['htmlTag'] . ">";             
-            }
-            
-            print( $htmlTagOpen . "<strong>" . $errorName . " </strong> " . $data['success'][$key]['value'] . $htmlTagClose . PHP_EOL);
-         }
-         
-         print( "</div> " . PHP_EOL );      
-      }   
+   { 
 /**
       echo ( "<Pre>" );
       print_r( $data );
