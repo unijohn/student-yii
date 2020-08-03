5447e4948c9e40627926b61a0539c1efa88b23bb
J Matthew Hall <jmhall@memphis.edu>
2020-08-03 14:16:33 -0500

    Should have used the 'mv' instead of saving/renaming within the text editor side.  See notes re: _users-view-add vs. _users-listing-add

---
+++ views/users/_users-listing-add.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\PostSearch */
+/* @var $form yii\widgets\ActiveForm */
+
+?>
+
+<div class="users-add-new-user">
+    <?php $form = ActiveForm::begin([
+        'action' => ['add'],
+        'method' => 'post',
+    ]); ?>
+
+    <?=  $form->field($model, 'uuid') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Add User',      ['class' => 'btn btn-primary',   'id' => 'addUserBtn',      'name' => 'User[addUser]',    'value' => 'addUser'    ]) ?>
+        <?= Html::submitButton('Lookup User',   ['class' => 'btn btn-info',      'id' => 'lookupUserBtn',   'name' => 'User[lookupUser]', 'value' => 'lookupUser' ]) ?>        
+
+<!--        
+        <?= Html::a('Reset', ['/users/index'],  ['class'=>'btn btn-default']) ?>
+  -->
+    </div>
+
+    <?php ActiveForm::end(); ?>
+</div>
