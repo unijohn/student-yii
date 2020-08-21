25bb60ddd474fd3e36fd927dc092857913c871ab
J Matthew Hall <jmhall@memphis.edu>
2020-08-21 14:05:48 -0500

    Another GH Action test - 5

--- models/FormFields.php
+++ models/FormFields.php
@@ -182,7 +182,7 @@ class FormFields extends BaseModel
         return ($count == 1 ? true : false);
     }
 
-//GH Test - 2020-003
+//GH Test - 2020-004
 
     /**
      * Determining if this entry already exists in the system by field properties
